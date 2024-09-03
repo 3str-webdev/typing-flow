65970005277085dfda8994dea0e0f7194322b792
3str-webdev <3str.webdev@gmail.com>
2024-09-03 08:38:19 +0300

    feat: add building by vite

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from "vite";
+import { resolve } from "node:path";
+import dts from "vite-plugin-dts";
+
+export default defineConfig({
+	build: {
+		lib: {
+			entry: resolve(__dirname, "src", "index.ts"),
+			name: "TypingFlow",
+			fileName: "index",
+			formats: ["es", "cjs"],
+		},
+	},
+	plugins: [dts({ insertTypesEntry: true })],
+});
